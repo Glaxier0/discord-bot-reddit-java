ea2d84536cab501edee141b0dd2d10563c853ca5
glaxier0 <tahir2001@hotmail.com>
2022-03-16 18:17:09 +0300

    Added new nsfw command and code optimization

---
+++ src/main/java/com/discord/bot/commands/nsfwcommands/NsfwCommandUtils.java
@@ -0,0 +1,45 @@
+package com.discord.bot.commands.nsfwcommands;
+
+import com.discord.bot.entity.Post;
+import com.discord.bot.entity.User;
+import com.discord.bot.service.UserService;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class NsfwCommandUtils {
+    UserService userService;
+
+    public NsfwCommandUtils(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void counter(String userId, String userWithTag) {
+        User user = userService.getUser(userId);
+        if (user == null) {
+            user = new User(userId, 0, 0, 0, 0, 0, userWithTag, 0);
+        }
+
+        user.setPCount(user.getPCount() + 1);
+
+        userService.save(user);
+    }
+
+    public void checkTypeAndPost(SlashCommandInteractionEvent event, Post post) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        if (post.getUrl().contains("redgifs.com")
+                || (post.getUrl().contains("imgur.com") && post.getUrl().contains(".gifv"))
+                || post.getUrl().contains("gfycat.com")) {
+            post.setContentType("video");
+        }
+
+        switch (post.getContentType()) {
+            case "video" -> event.reply(post.getUrl()).queue();
+            case "image" -> {
+                embedBuilder.setTitle(post.getTitle(), post.getPermaUrl())
+                        .setImage(post.getUrl())
+                        .setFooter("Posted in r/" + post.getSubreddit() + " by u/" + post.getAuthor());
+                event.replyEmbeds(embedBuilder.build()).queue();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/discord/bot/commands/nsfwcommands/TitsCommand.java
@@ -0,0 +1,37 @@
+package com.discord.bot.commands.nsfwcommands;
+
+import com.discord.bot.commands.ISlashCommand;
+import com.discord.bot.entity.Post;
+import com.discord.bot.service.PostService;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Random;
+
+public class TitsCommand implements ISlashCommand {
+    PostService postService;
+    NsfwCommandUtils utils;
+
+    Random random = new Random();
+
+    public TitsCommand(PostService postService, NsfwCommandUtils utils) {
+        this.postService = postService;
+        this.utils = utils;
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        if (event.getTextChannel().isNSFW()) {
+            List<Post> postList = postService.getTits();
+            Post post = postList.get(random.nextInt(postList.size()));
+            utils.checkTypeAndPost(event, post);
+            net.dv8tion.jda.api.entities.User user = event.getUser();
+            utils.counter(user.getId(), user.getAsTag());
+        } else {
+            event.replyEmbeds(new EmbedBuilder().setDescription("Channel is not nsfw. Please write command in nsfw channel.")
+                    .setColor(Color.RED).build()).queue();
+        }
+    }
+}
