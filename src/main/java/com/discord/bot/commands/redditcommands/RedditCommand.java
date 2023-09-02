55cc2a6d0cd945a9ad2e2bc15ce7c68e15630979
Tahir Çelik <tahir2001@hotmail.com>
2023-09-02 16:46:14 +0300

    added generic reddit command

--- src/main/java/com/discord/bot/commands/redditcommands/RedditCommand.java
+++ src/main/java/com/discord/bot/commands/redditcommands/RedditCommand.java
@@ -3,22 +3,26 @@ package com.discord.bot.commands.redditcommands;
 import com.discord.bot.commands.ISlashCommand;
 import com.discord.bot.entity.Post;
 import com.discord.bot.service.PostService;
+import com.discord.bot.service.SubredditService;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 import java.util.List;
 
-public class FacepalmCommand implements ISlashCommand {
+public class RedditCommand implements ISlashCommand {
     PostService postService;
+    SubredditService subredditService;
     RedditCommandUtils utils;
 
-    public FacepalmCommand(PostService postService, RedditCommandUtils utils) {
+    public RedditCommand(PostService postService, SubredditService subredditService, RedditCommandUtils utils) {
         this.postService = postService;
+        this.subredditService = subredditService;
         this.utils = utils;
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        List<Post> list = postService.getPosts("facepalm");
+        String subreddit = event.getFullCommandName();
+        List<Post> list = postService.getPosts(subreddit);
         utils.checkTypeAndPost(event, list);
         net.dv8tion.jda.api.entities.User user = event.getUser();
         utils.counter(user.getId(), user.getAsTag());
