35831b1fc6dfb7ee8fc971096859eae340b08026
Tahir <tahirclk@hotmail.com>
2024-02-17 02:26:30 +0300

    refactor: entities

--- src/main/java/com/discord/bot/Entity/Subreddit.java
+++ src/main/java/com/discord/bot/Entity/Subreddit.java
@@ -1,12 +1,9 @@
 package com.discord.bot.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import jakarta.persistence.*;
 
-import javax.persistence.*;
-
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(name = "subreddits")
 @NoArgsConstructor
